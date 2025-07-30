119dcfd83734abb478c993f92657cb71007b46d0
juanfferb <juanffernandezb@javeriana.edu.co>
2025-07-29 20:08:25 -0500

    Funcionalidad Búsquedas en Angular

--- tesis/src/main/java/com/grupo7/tesis/controller/RagController.java
+++ tesis/src/main/java/com/grupo7/tesis/controller/RagController.java
@@ -5,6 +5,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/rag")
+@CrossOrigin(origins = "*") // Permite el acceso desde cualquier frontend (útil en desarrollo)
+public class RagController {
+
+    private final RagService ragService;
+
+    @Autowired
+    public RagController(RagService ragService) {
+        this.ragService = ragService;
+    }
+
+    @PostMapping
+    public String consultarRag(@RequestBody QuestionDTO question) {
+        return ragService.obtenerRespuestaRag(question.getQuestion());
+    }
+
+    // Clase estática interna que actúa como DTO para recibir el cuerpo del POST
+    public static class QuestionDTO {
+        private String question;
+
+        public String getQuestion() {
+            return question;
+        }
+
+        public void setQuestion(String question) {
+            this.question = question;
+        }
+    }
+}
+
+/*package com.grupo7.tesis.controller;
+
+import com.grupo7.tesis.service.RagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 @RequestMapping("/rag")
 @CrossOrigin(origins = "*") // Permite acceso desde el frontend
 public class RagController {
@@ -20,4 +57,4 @@ public class RagController {
     public String consultaRag(@RequestBody String pregunta) {
         return ragService.obtenerRespuestaRag(pregunta);
     }
-}
+}*/
