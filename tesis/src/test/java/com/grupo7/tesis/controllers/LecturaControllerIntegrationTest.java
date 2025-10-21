ee8908d7bbff4d33ad772d174604dd8e9ce20971
Santiago Ortiz <106709063+Santi-Ortiz@users.noreply.github.com>
2025-10-21 12:09:40 -0500

    Plantilla de archivos para pruebas de integración

---
+++ tesis/src/test/java/com/grupo7/tesis/controllers/LecturaControllerIntegrationTest.java
@@ -0,0 +1,8 @@
+package com.grupo7.tesis.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+    
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class LecturaControllerIntegrationTest {
+
+}
